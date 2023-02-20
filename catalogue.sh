7fc67e451eef5b2591dce8448a2cd7dde1b15fc2
Siva Kumar <sivakumar9399959803@gmail.com>
2023-02-20 17:34:38 +0530

    catalogue in roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -1,21 +1,50 @@
 source common.sh
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app
-rm -rf /app/*
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-cp configs/catalogue.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+print_head "Configure Nodejs Repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
-cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
+print_head "Install Nodejs"
+yum install nodejs -y &>>${log_file}
 
-mongo --host mongodb.devops71.online </app/schema/catalogue.js
+print_head "Create Roboshop User"
+useradd roboshop &>>${log_file}
+
+print_head "Create Application Directory"
+mkdir /app &>>${log_file}
+
+print_head "Delete Old Content"
+rm -rf /app/* &>>${log_file}
+
+print_head "Downloading App Content"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
+cd /app
+
+print_head "Extracting App Content"
+unzip /tmp/catalogue.zip &>>${log_file}
+
+print_head " Installing Nodejs App Content"
+npm install &>>${log_file}
+
+print_head "Copy SystemD Service File"
+cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+
+print_head "Reload SystemD"
+systemctl daemon-reload &>>${log_file}
+
+print_head "Enable Catalogue Service"
+systemctl enable catalogue &>>${log_file}
+
+print_head "Start Catalogue Service"
+systemctl restart catalogue &>>${log_file}
+
+print_head "Copy MongoDB Repo File"
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+
+print_head "Install MongoDB Client"
+yum install mongodb-org-shell -y &>>${log_file}
+
+print_head "Load Schema"
+mongo --host mongodb.devops71.online </app/schema/catalogue.js &>>${log_file}
+
+
+## To check error in file we use cat /tmp/roboshop.log
