3a255d0c463da837810b628bf0bff81c86628212
Siva Kumar <sivakumar9399959803@gmail.com>
2023-02-21 17:32:08 +0530

    redis in roboshop

---
+++ redis.sh
@@ -0,0 +1,25 @@
+source common.sh
+
+print_head "Installing Redis Repo File"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log_file}
+status_check $?
+
+print_head "Enable 6.2 redis repo"
+dnf module enable redis:remi-6.2 -y &>>${log_file}
+status_check $?
+
+print_head "Install Redis"
+yum install redis -y &>>${log_file}
+status_check $?
+
+print_head "update Redis Listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' vim /etc/redis.conf /etc/redis/redis.conf &>>${log_file}
+status_check $?
+
+print_head "Enable redis"
+systemctl enable redis &>>${log_file}
+status_check $?
+
+print_head "start redis"
+systemctl start redis &>>${log_file}
+status_check $?
