be99bdaf86617439daef2e499240955bb7a39cc9
Siva Kumar <sivakumar9399959803@gmail.com>
2023-02-21 16:53:19 +0530

    catalogue in roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -2,49 +2,68 @@ source common.sh
 
 print_head "Configure Nodejs Repo"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+status_check $?
 
 print_head "Install Nodejs"
 yum install nodejs -y &>>${log_file}
+status_check $?
 
 print_head "Create Roboshop User"
-useradd roboshop &>>${log_file}
+id roboshop &>>${log_file}
+if [ $? -ne 0]; then
+  useradd roboshop &>>${log_file}
+fi
+status_check $?
 
 print_head "Create Application Directory"
-mkdir /app &>>${log_file}
+if [ ! -d /app]; then
+   mkdir /app &>>${log_file}
+fi
+status_check $?
 
 print_head "Delete Old Content"
 rm -rf /app/* &>>${log_file}
+status_check $?
 
 print_head "Downloading App Content"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
 cd /app
+status_check $?
 
 print_head "Extracting App Content"
 unzip /tmp/catalogue.zip &>>${log_file}
+status_check $?
 
 print_head " Installing Nodejs App Content"
 npm install &>>${log_file}
+status_check $?
 
 print_head "Copy SystemD Service File"
 cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+status_check $?
 
 print_head "Reload SystemD"
 systemctl daemon-reload &>>${log_file}
+status_check $?
 
 print_head "Enable Catalogue Service"
 systemctl enable catalogue &>>${log_file}
+status_check $?
 
 print_head "Start Catalogue Service"
 systemctl restart catalogue &>>${log_file}
+status_check $?
 
 print_head "Copy MongoDB Repo File"
 cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+status_check $?
 
 print_head "Install MongoDB Client"
 yum install mongodb-org-shell -y &>>${log_file}
+status_check $?
 
 print_head "Load Schema"
 mongo --host mongodb.devops71.online </app/schema/catalogue.js &>>${log_file}
-
+status_check $?
 
 ## To check error in file we use cat /tmp/roboshop.log
