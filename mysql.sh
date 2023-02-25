fe4301d2222dc9b333802658cd3ddbae82ec7a42
Siva Kumar <sivakumar9399959803@gmail.com>
2023-02-25 14:00:01 +0530

    mysql in roboshop

--- mysql.sh
+++ mysql.sh
@@ -12,7 +12,7 @@ dnf module disable mysql -y &>>${log_file}
 status_check $?
 
 print_head "copy Mysql repo file"
-cp ${code_dir}/configs/mysql.repo /etc?yum.repos.d/mysql.repo &>>${log_file}
+cp ${code_dir}/configs/mysql.repo /etc/yum.repos.d/mysql.repo &>>${log_file}
 status_check $?
 
 print_head "Installing Mysql server"
