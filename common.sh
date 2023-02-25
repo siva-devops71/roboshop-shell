233360852ad8b5e7b6eb14d52bac98de4d79e899
Siva Kumar <sivakumar9399959803@gmail.com>
2023-02-25 12:01:30 +0530

    user in roboshop

--- common.sh
+++ common.sh
@@ -17,3 +17,72 @@ status_check() {
   fi
 
 }
+
+NODEJS() {
+  print_head "Configure Nodejs Repo"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+  status_check $?
+
+  print_head "Install Nodejs"
+  yum install nodejs -y &>>${log_file}
+  status_check $?
+
+  print_head "Create Roboshop User"
+  id roboshop &>>${log_file}
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>${log_file}
+  fi
+  status_check $?
+
+  print_head "Create Application Directory"
+  if [ ! -d /app ]; then
+     mkdir /app &>>${log_file}
+  fi
+  status_check $?
+
+  print_head "Delete Old Content"
+  rm -rf /app/* &>>${log_file}
+  status_check $?
+
+  print_head "Downloading App Content"
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+  cd /app
+  status_check $?
+
+  print_head "Extracting App Content"
+  unzip /tmp/${component}.zip &>>${log_file}
+  status_check $?
+
+  print_head " Installing Nodejs App Content"
+  npm install &>>${log_file}
+  status_check $?
+
+  print_head "Copy SystemD Service File"
+  cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+  status_check $?
+
+  print_head "Reload SystemD"
+  systemctl daemon-reload &>>${log_file}
+  status_check $?
+
+  print_head "Enable ${component} Service"
+  systemctl enable ${component} &>>${log_file}
+  status_check $?
+
+  print_head "Start ${component} Service"
+  systemctl restart ${component} &>>${log_file}
+  status_check $?
+
+  print_head "Copy MongoDB Repo File"
+  cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+  status_check $?
+
+  print_head "Install MongoDB Client"
+  yum install mongodb-org-shell -y &>>${log_file}
+  status_check $?
+
+  print_head "Load Schema"
+  mongo --host mongodb.devops71.online </app/schema/${component}1.js &>>${log_file}
+  status_check $?
+
+}
