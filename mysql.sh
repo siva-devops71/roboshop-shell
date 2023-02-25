8d945ddca4b87cc9ffa245493dadd8d566a51196
Siva Kumar <sivakumar9399959803@gmail.com>
2023-02-25 14:07:51 +0530

    mysql in roboshop

--- mysql.sh
+++ mysql.sh
@@ -29,8 +29,8 @@ status_check $?
 
 
 print_head "Set Root Password"
-echo show databases | mysql -uroot -p{mysql_root_password} &>>${log_file}
+echo show databases | mysql -uroot -p${mysql_root_password} &>>${log_file}
 if [ $? -ne 0 ]; then
-    mysql_secure_installation --set-root-pass {mysql_root_password} &>>${log_file}
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log_file}
 fi
 status_check $?
