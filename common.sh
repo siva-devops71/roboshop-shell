f061bc80848f761d666f5918b2ce2c8c872547dd
Siva Kumar <sivakumar9399959803@gmail.com>
2023-02-25 15:00:09 +0530

    Update Roboshop-shell

--- common.sh
+++ common.sh
@@ -6,7 +6,6 @@ print_head(){
   echo -e "\e[35m$1\e[0m"
 }
 
-
 status_check() {
   if [ $1 -eq 0 ]; then
     echo SUCCESS
@@ -15,12 +14,28 @@ status_check() {
     echo "Read the log file ${log_file} for more information about error"
     exit 1
   fi
+}
 
+systemd_setup() {
+    print_head "Copy SystemD Service File"
+    cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+    status_check $?
+
+    print_head "Reload SystemD"
+    systemctl daemon-reload &>>${log_file}
+    status_check $?
+
+    print_head "Enable ${component} Service"
+    systemctl enable ${component} &>>${log_file}
+    status_check $?
+
+    print_head "Start ${component} Service"
+    systemctl restart ${component} &>>${log_file}
+    status_check $?
 }
 
 schema_setup() {
   if [ "${schema_type}" == "mongo" ]; then
-
     print_head "Copy MongoDB Repo File"
     cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
     status_check $?
@@ -32,10 +47,44 @@ schema_setup() {
     print_head "Load Schema"
     mongo --host mongodb.devops71.online </app/schema/${component}.js &>>${log_file}
     status_check $?
+  elifi [ "${schema_type}" == "mysql" ]; then
+    print_head "Install Mysql Client"
+    yum install mysql -y &>>${log_file}
+    status_check $?
 
-    fi
+    print_head "Load Schema"
+    mysql -h mysql.devops71.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql &>>${log_file}
+    status_check $?
+  fi
 }
 
+app_prereq_setup() {
+    print_head "Create Roboshop User"
+    id roboshop &>>${log_file}
+     if [ $? -ne 0 ]; then
+      useradd roboshop &>>${log_file}
+     fi
+     status_check $?
+
+     print_head "Create Application Directory"
+     if [ ! -d /app ]; then
+       mkdir /app &>>${log_file}
+     fi
+     status_check $?
+
+     print_head "Delete Old Content"
+     rm -rf /app/* &>>${log_file}
+     status_check $?
+
+     print_head "Downloading App Content"
+     curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+     cd /app
+     status_check $?
+
+     print_head "Extracting App Content"
+     unzip /tmp/${component}.zip &>>${log_file}
+     status_check $?
+}
 
 nodejs() {
   print_head "Configure Nodejs Repo"
@@ -46,52 +95,48 @@ nodejs() {
   yum install nodejs -y &>>${log_file}
   status_check $?
 
-  print_head "Create Roboshop User"
-  id roboshop &>>${log_file}
-  if [ $? -ne 0 ]; then
-    useradd roboshop &>>${log_file}
-  fi
-  status_check $?
-
-  print_head "Create Application Directory"
-  if [ ! -d /app ]; then
-     mkdir /app &>>${log_file}
-  fi
-  status_check $?
-
-  print_head "Delete Old Content"
-  rm -rf /app/* &>>${log_file}
-  status_check $?
-
-  print_head "Downloading App Content"
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
-  cd /app
-  status_check $?
-
-  print_head "Extracting App Content"
-  unzip /tmp/${component}.zip &>>${log_file}
-  status_check $?
+  app_prereq_setup
 
   print_head " Installing Nodejs App Content"
   npm install &>>${log_file}
   status_check $?
 
-  print_head "Copy SystemD Service File"
-  cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+  schema_setup
+
+  systemd_setup
+
+}
+
+
+java() {
+  print_head "Install Maven"
+  yum install maven -y &>>${log_file}
   status_check $?
 
-  print_head "Reload SystemD"
-  systemctl daemon-reload &>>${log_file}
+  app_prereq_setup
+
+  print_head "Download Dependencies & Package"
+  mvn clean package &>>${log_file}
+  mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
+  status_check $?
+   # Schema Setup Function
+  schema_setup
+  # SystemD Function
+  systemd_setup
+}
+
+python() {
+
+  print_head "Install Python"
+  yum install python36 gcc python3-devel -y &>>${log_file}
   status_check $?
 
-  print_head "Enable ${component} Service"
-  systemctl enable ${component} &>>${log_file}
+  app_prereq_setup
+
+  print_head "Download Dependencies"
+  pip3.6 install -r requirements.txt &>>${log_file}
   status_check $?
 
-  print_head "Start ${component} Service"
-  systemctl restart ${component} &>>${log_file}
-  status_check $?
-
-schema_setup
-
+  # SystemD Function
+  systemd_setup
 }
